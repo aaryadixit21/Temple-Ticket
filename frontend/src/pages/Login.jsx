04f823acc7decd52e551df5bb9fde791f07a5c89
aaryadixit21 <dixitaarya3@gmail.com>
2024-03-30 12:57:35 +0530

    login and register pages created

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,37 @@
-const Login = () =>{
-  return (
-    <div>
-      <h3>Login</h3>
-    </div>
-  );
+import { Link } from "react-router-dom";
+
+export default function Login() {
+    return (
+        <div>
+            <div>
+                <h4>Login</h4>
+                <form>
+                    
+                    <div>
+                        <label htmlFor="email"></label>
+                        <input
+                            type="text"
+                            placeholder="email"
+                            autoComplete="off"
+                            name="email"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="password"></label>
+                        <input
+                            type="password"
+                            placeholder="password"
+                            name="password"
+                            id="password" 
+                        />
+                    </div>
+                    <button type="submit">Login</button>
+                    <p>New to this website ?</p>
+                    <Link to="/register">
+                        <button>Register new account</button>
+                    </Link>
+                </form>
+            </div>
+        </div>
+    );
 }
-export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,56 @@
-const Register = () =>{
+import { Link, useLocation } from "react-router-dom";
+
+export default function Login() {
+  const location = useLocation();
+
   return (
     <div>
-      <h3>Register</h3>
+        <h2>Register</h2>
+      <form>
+        <div>
+          <label htmlFor="username"> </label>
+          <input
+            type="text"
+            placeholder="First Name"
+            autoComplete="off"
+            name="First Name"
+          />
+        </div>
+        <div>
+          <label htmlFor="username"> </label>
+          <input
+            type="text"
+            placeholder="Last Name"
+            autoComplete="off"
+            name="Last Name"
+          />
+        </div>
+        <div>
+          <label htmlFor="email"></label>
+          <input
+            type="email"
+            placeholder="email"
+            name="email"
+            id="email"
+          />
+        </div>
+        <div>
+          <label htmlFor="password"></label>
+          <input
+            type="password"
+            placeholder="password"
+            name="password"
+            id="password"
+          />
+        </div>
+        {location.pathname === '/register' && (
+          <button type="submit">Register</button>
+        )}
+      </form>
+        <h5>Already have an account ?</h5>
+        <Link to="/login">
+            <button>Log in</button>
+        </Link>
     </div>
   );
 }
-export default Register;
